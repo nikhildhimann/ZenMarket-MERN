cfbe24cf4d28161381f5e62bb11db127508db6c7
Nikhil Dhiman <nikhildhimaann@outlook.com>
2025-10-25 21:58:13 +0530

    fix url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on the current mode (e.g., development or production)
-  const env = loadEnv(mode, process.cwd())
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL, // use env variable from .env file
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
 })
